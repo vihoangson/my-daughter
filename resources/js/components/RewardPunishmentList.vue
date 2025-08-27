2ae377605f43b4b61a1afa8630d698e3d68f8484
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2025-08-27 15:08:16 +0700

    update

--- resources/js/components/RewardPunishmentList.vue
+++ resources/js/components/RewardPunishmentList.vue
@@ -1,32 +1,35 @@
 <template>
   <div>
-    <h2>Reward & Punishment List</h2>
-    <button class="btn btn-primary mb-2" @click="$emit('edit', null)">Add New</button>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Child</th>
-          <th>Type</th>
-          <th>Points</th>
-          <th>Description</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in data" :key="item.id">
-          <td>{{ item.id }}</td>
-          <td>{{ item.child?.name }}</td>
-          <td>{{ item.type }}</td>
-          <td>{{ item.points }}</td>
-          <td>{{ item.description }}</td>
-          <td>
-            <button class="btn btn-sm btn-warning me-2" @click="$emit('edit', item)">Edit</button>
-            <button class="btn btn-sm btn-danger" @click="handleDelete(item.id)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="d-none">
+          <h2>Reward & Punishment List</h2>
+          <button class="btn btn-primary mb-2" @click="$emit('edit', null)">Add New</button>
+          <table class="table table-bordered d-none">
+              <thead>
+              <tr>
+                  <th>ID</th>
+                  <th>Child</th>
+                  <th>Type</th>
+                  <th>Points</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="item in data" :key="item.id">
+                  <td>{{ item.id }}</td>
+                  <td>{{ item.child?.name }}</td>
+                  <td>{{ item.type }}</td>
+                  <td>{{ item.points }}</td>
+                  <td>{{ item.description }}</td>
+                  <td>
+                      <button class="btn btn-sm btn-warning me-2" @click="$emit('edit', item)">Edit</button>
+                      <button class="btn btn-sm btn-danger" @click="handleDelete(item.id)">Delete</button>
+                  </td>
+              </tr>
+              </tbody>
+          </table>
+      </div>
+
   </div>
 </template>
 
